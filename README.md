74e2b0bde194c7047978daa6ba90249956edef37
prashishjungrai <prashishjungrai23@gmail.com>
2025-09-05 14:19:35 +0545

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Medical-chatbot
+
+```bash
+conda create -n medibot python=3.12 -y
+```
+```bash
+conda active medibot
+```
+### step2-intall the requirements
+```bash
+pip install -r requirements.txt
+```
---
+++ template.py
@@ -0,0 +1,32 @@
+#create entire project template. write entire logic to create a structure
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')   
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb",
+    "test.py",# add a jupyter notebook file
+  
+]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as fp:
+            pass
+        logging.info(f"Creating empty file: {filename}")
+    else:
+        logging.info(f"File already exists: {filename}")
